7c52c2281253f34779cb392132338f1f3eb81cc1
RealMarkSussex <mark.sussex@gocompare.com>
2020-03-31 09:33:01 +0100

    added excel class (having file not found exception)

--- StarWarsBackend/StarWarsBackend/Controllers/ExcelController.cs
+++ StarWarsBackend/StarWarsBackend/Controllers/ExcelController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using GemBox.Spreadsheet;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using StarWarsBackend.Models;
@@ -19,7 +21,11 @@ namespace StarWarsBackend.Controllers
             {
                 return BadRequest();
             }
-            return Ok();
+            var options = SaveOptions.XlsxDefault;
+            var noSpaceName = Regex.Replace(person.Name, @"s", "");
+            var fileName = noSpaceName + "profile.xlsx";
+            var excelDocument = new Excel(person);
+            return File(excelDocument.Create(), options.ContentType, fileName);
         }
     }
 }
